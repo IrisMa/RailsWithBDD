3e8e215f206bc1754f61e53b3469522a662b3e8a
YangBo <yangbodotnet@gmail.com>
2011-12-02 15:01:15 +0800

    first commit all rails project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWithBDD::Application.config.secret_token = '09816f059a290417cdda0e290d871c81cb3f43e310e9351923e7af2322cd16c43805ea808d807baaaea3367c833916a60dbabad7f761b8039d29825d63426e66'
---
+++ features/step_definitions/rails_with_bdd_description.rb
@@ -0,0 +1,9 @@
+Given /^a movie$/ do
+  pending
+end
+When /^I set the showtime to "2007\-10\-10"([^"]*)"2:15pm"$/ do |arg1|
+  pending
+end
+Then /^the showtime description should be "October (\d+), (\d+) \(2:15pm\)"$/ do |arg1, arg2|
+  pending
+end
